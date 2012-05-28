e3b43ec84a41d941af16fd88b6d0397fa5cf837e
Kirill Šustov <kirill.sustov@actumg2.cz>
2012-05-28 18:12:10 +0200

    Removed legacy constants

--- tests/EmailTestHelper.php
+++ tests/EmailTestHelper.php
@@ -10,20 +10,6 @@ namespace Actum\Utils;
 
 class EmailTestHelper {
 
-	const ISEMAIL_META_DESC = 1;        // Explanatory description of this diagnosis or category
-	const ISEMAIL_META_CONSTANT = 2;    // The name of the constant for this diagnosis or category
-	const ISEMAIL_META_SMTP = 4;        // The SMTP enhanced status message for this diagnosis (the bounce message)
-	const ISEMAIL_META_CATEGORY = 8;    // The category name for this diagnosis
-	const ISEMAIL_META_CAT_VALUE = 16;  // The category value for this diagnosis
-	const ISEMAIL_META_CAT_DESC = 32;   // The category description for this diagnosis
-	const ISEMAIL_META_REF_TEXT = 64;   // Any supporting text associated with the diagnosis or category
-	const ISEMAIL_META_REF_CITE = 128;  // Document cited to support this diagnosis or category
-	const ISEMAIL_META_REF_LINK = 256;  // Link to any supporting material associated with the diagnosis or category
-	const ISEMAIL_META_REF_HTML = 512;  // Complete HTML for any supporting material associated with the diagnosis or category
-	const ISEMAIL_META_REF_ALT = 1024;  // Complete HTML for any supporting material associated with the diagnosis or category
-	const ISEMAIL_META_VALUE = 2048;    // Numeric value of the diagnosis or category
-	const ISEMAIL_META_USEFUL = 1071;   // A array containing a useful set of analysis data
-	const ISEMAIL_META_ALL = 4095;      // An array containing all available analysis of this dignosis
 	const ISEMAIL_STRING_UNKNOWN = '(unknown)';
 
 	private $metaPath = '../is_email/test/meta.xml';
