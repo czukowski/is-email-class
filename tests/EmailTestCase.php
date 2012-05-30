0dbb5d7217da77c14f8bc19b397b7e7e0048341a
Kirill Šustov <kirill.sustov@actumg2.cz>
2012-05-30 07:17:14 +0200

    Added decoding values from XML Updated phpDocs

--- tests/EmailTestCase.php
+++ tests/EmailTestCase.php
@@ -12,6 +12,8 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 
 	private $helper;
 	private $hasInternet;
+	private $replaceChars;
+	private $searchChars;
 
 	public function provideEmails() {
 		$provide = array();
@@ -28,20 +30,39 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 		}
 		$formallyValid = $isValid <= Email::ISEMAIL_RFC5321_IPV6DEPRECATED;
 		return array(
-			(string) $testCase->address,
+			$this->getDecodedString($testCase->address),
 			$this->hasInternet(),
 			$isValid,
 			$formallyValid,
-			(string) $testCase->comment,
+			$this->getDecodedString($testCase->comment),
 		);
 	}
 
+	/**
+	 * Certain email address characters in XML files are encoded, this method will decode them
+	 * 
+	 * @param   mixed  $value
+	 * @return  string
+	 */
+	private function getDecodedString($value) {
+		return str_replace($this->getSearch(), $this->getReplace(), (string) $value);
+	}
+
+	/**
+	 * Retrieves diagnosis by its ID (constant name)
+	 * 
+	 * @param   mixed   $constantName
+	 * @return  integer
+	 */
 	protected function getExpectedDiagnosis($constantName) {
 		$diagnosis = $this->getHelper()
 			->getAnalysis($constantName);
 		return $diagnosis['value'];
 	}
 
+	/**
+	 * @return  EmailTestHelper 
+	 */
 	protected function getHelper() {
 		if ($this->helper === NULL) {
 			$this->helper = new EmailTestHelper;
@@ -49,6 +70,9 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 		return $this->helper;
 	}
 
+	/**
+	 * @return  boolean
+	 */
 	protected function hasInternet() {
 		if ($this->hasInternet === NULL) {
 			// The @ operator is used here to avoid DNS errors when there is no connection.
@@ -69,7 +93,7 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
     }
 
     /**
-     * Returns domain part fro mthe email address
+     * Returns domain part from the email address
      * 
      * @param   string  $email
      * @return  string
@@ -77,4 +101,37 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
     private function getEmailDomain($email) {
         return substr($email, strrpos($email, '@') + 1);
     }
+
+	/**
+	 * Can't store ASCII or Unicode characters below U+0020 in XML file so we put a token in the XML
+	 * (except for HTAB, CR & LF)
+	 * The tokens we have chosen are the Unicode Characters 'SYMBOL FOR xxx' (U+2400 onwards)
+	 * Here we prepare the symbols mapping to the actual characters.
+	 */
+	private function prepareDecodeParameters() {
+		$needles = array(' ', mb_convert_encoding('&#9229;&#9226;', 'UTF-8', 'HTML-ENTITIES'));
+		$substitutes = array(' ', chr(13).chr(10));
+		for ($i = 0; $i < 32; $i++) {
+			// PHP bug doesn't allow us to use hex notation (http://bugs.php.net/48645)
+			$entity = mb_convert_encoding('&#'.(string) (9216 + $i).';', 'UTF-8', 'HTML-ENTITIES');
+			$needles[] = $entity;
+			$substitutes[] = chr($i);
+		}
+		$this->searchChars = $needles;
+		$this->replaceChars	= $substitutes;
+	}
+
+	private function getSearch() {
+		if ($this->searchChars === NULL) {
+			$this->prepareDecodeParameters();
+		}
+		return $this->searchChars;
+	}
+
+	private function getReplace() {
+		if ($this->replaceChars === NULL) {
+			$this->prepareDecodeParameters();
+		}
+		return $this->replaceChars;
+	}
 }
