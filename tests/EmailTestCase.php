6edb73386d2b957086f4b8e299a5dfffcc33043b
Kirill Šustov <kirill.sustov@actumg2.cz>
2012-05-30 17:13:11 +0200

    Moved more logic to test helper

--- tests/EmailTestCase.php
+++ tests/EmailTestCase.php
@@ -11,9 +11,6 @@ namespace Actum\Utils;
 abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 
 	private $helper;
-	private $hasInternet;
-	private $replaceChars;
-	private $searchChars;
 
 	/**
 	 * @dataProvider  provideEmails
@@ -38,54 +35,8 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 	 * Provides test cases from XML files
 	 */
 	public function provideEmails() {
-		$provide = array();
-		foreach ($this->getHelper()->getTestCases() as $testCase) {
-			$provide[ (string) $testCase->attributes()->id] = $this->createTestCase($testCase);
-		}
-		return $provide;
-	}
-
-	/**
-	 * Formats XML element to test case array
-	 * 
-	 * @param   \SimpleXMLElement  $testCase
-	 * @return  array
-	 */
-	private function createTestCase(\SimpleXMLElement $testCase) {
-		$isValid = $this->getExpectedDiagnosis( (string) $testCase->diagnosis);
-		if ( ! $this->hasInternet() && $isValid <= Email::ISEMAIL_DNSWARN) {
-			$isValid = Email::ISEMAIL_VALID;
-		}
-		$formallyValid = $isValid <= Email::ISEMAIL_RFC5321_IPV6DEPRECATED;
-		return array(
-			$this->getDecodedString($testCase->address),
-			$this->hasInternet(),
-			$isValid,
-			$formallyValid,
-			$this->getDecodedString($testCase->comment),
-		);
-	}
-
-	/**
-	 * Certain email address characters in XML files are encoded, this method will decode them
-	 * 
-	 * @param   mixed  $value
-	 * @return  string
-	 */
-	private function getDecodedString($value) {
-		return str_replace($this->getSearch(), $this->getReplace(), (string) $value);
-	}
-
-	/**
-	 * Retrieves diagnosis by its ID (constant name)
-	 * 
-	 * @param   mixed   $constantName
-	 * @return  integer
-	 */
-	protected function getExpectedDiagnosis($constantName) {
-		$diagnosis = $this->getHelper()
-			->getAnalysis($constantName);
-		return $diagnosis['value'];
+		return $this->getHelper()
+			->getTestCases();
 	}
 
 	/**
@@ -96,18 +47,6 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
 			$this->helper = new EmailTestHelper;
 		}
 		return $this->helper;
-	}
-
-	/**
-	 * @return  boolean
-	 */
-	protected function hasInternet() {
-		if ($this->hasInternet === NULL) {
-			// The @ operator is used here to avoid DNS errors when there is no connection.
-			$sock = @fsockopen("www.google.com", 80, $errno, $errstr, 1);
-			$this->hasInternet = (bool) $sock ? TRUE : FALSE;
-		}
-		return $this->hasInternet;
 	}
 
     /**
@@ -129,37 +68,4 @@ abstract class EmailTestCase extends \PHPUnit_Framework_TestCase {
     private function getEmailDomain($email) {
         return substr($email, strrpos($email, '@') + 1);
     }
-
-	/**
-	 * Can't store ASCII or Unicode characters below U+0020 in XML file so we put a token in the XML
-	 * (except for HTAB, CR & LF)
-	 * The tokens we have chosen are the Unicode Characters 'SYMBOL FOR xxx' (U+2400 onwards)
-	 * Here we prepare the symbols mapping to the actual characters.
-	 */
-	private function prepareDecodeParameters() {
-		$needles = array(' ', mb_convert_encoding('&#9229;&#9226;', 'UTF-8', 'HTML-ENTITIES'));
-		$substitutes = array(' ', chr(13).chr(10));
-		for ($i = 0; $i < 32; $i++) {
-			// PHP bug doesn't allow us to use hex notation (http://bugs.php.net/48645)
-			$entity = mb_convert_encoding('&#'.(string) (9216 + $i).';', 'UTF-8', 'HTML-ENTITIES');
-			$needles[] = $entity;
-			$substitutes[] = chr($i);
-		}
-		$this->searchChars = $needles;
-		$this->replaceChars	= $substitutes;
-	}
-
-	private function getSearch() {
-		if ($this->searchChars === NULL) {
-			$this->prepareDecodeParameters();
-		}
-		return $this->searchChars;
-	}
-
-	private function getReplace() {
-		if ($this->replaceChars === NULL) {
-			$this->prepareDecodeParameters();
-		}
-		return $this->replaceChars;
-	}
 }
--- tests/EmailTestHelper.php
+++ tests/EmailTestHelper.php
@@ -12,6 +12,9 @@ class EmailTestHelper {
 
 	const ISEMAIL_STRING_UNKNOWN = '(unknown)';
 
+	private $hasInternet;
+	private $replaceChars;
+	private $searchChars;
 	private $metaPath = '../is_email/test/meta.xml';
 	private $xmlPath = '../is_email/test/tests.xml';
 	private $xmlCache = array();
@@ -181,7 +184,11 @@ class EmailTestHelper {
 	 * @return  \SimpleXMLIterator
 	 */
 	public function getTestCases() {
-		return $this->getXml($this->xmlPath)->test;
+		$provide = array();
+		foreach ($this->getXml($this->xmlPath)->test as $testCase) {
+			$provide[ (string) $testCase->attributes()->id] = $this->createTestCase($testCase);
+		}
+		return $provide;
 	}
 
 	/**
@@ -195,5 +202,92 @@ class EmailTestHelper {
 			}
 		}
 		return $this->xmlCache[$relativePath];
+	}
+
+	/**
+	 * Formats XML element to test case array
+	 * 
+	 * @param   \SimpleXMLElement  $testCase
+	 * @return  array
+	 */
+	private function createTestCase(\SimpleXMLElement $testCase) {
+		$isValid = $this->getExpectedDiagnosis( (string) $testCase->diagnosis);
+		if ( ! $this->hasInternet() && $isValid <= Email::ISEMAIL_DNSWARN) {
+			$isValid = Email::ISEMAIL_VALID;
+		}
+		$formallyValid = $isValid <= Email::ISEMAIL_RFC5321_IPV6DEPRECATED;
+		return array(
+			$this->getDecodedString($testCase->address),
+			$this->hasInternet(),
+			$isValid,
+			$formallyValid,
+			$this->getDecodedString($testCase->comment),
+		);
+	}
+
+	/**
+	 * Retrieves diagnosis by its ID (constant name)
+	 * 
+	 * @param   mixed   $constantName
+	 * @return  integer
+	 */
+	private function getExpectedDiagnosis($constantName) {
+		$diagnosis = $this->getAnalysis($constantName);
+		return $diagnosis['value'];
+	}
+
+	/**
+	 * Certain email address characters in XML files are encoded, this method will decode them
+	 * 
+	 * @param   mixed  $value
+	 * @return  string
+	 */
+	private function getDecodedString($value) {
+		return str_replace($this->getSearch(), $this->getReplace(), (string) $value);
+	}
+
+	private function getSearch() {
+		if ($this->searchChars === NULL) {
+			$this->prepareDecodeParameters();
+		}
+		return $this->searchChars;
+	}
+
+	private function getReplace() {
+		if ($this->replaceChars === NULL) {
+			$this->prepareDecodeParameters();
+		}
+		return $this->replaceChars;
+	}
+
+	/**
+	 * Can't store ASCII or Unicode characters below U+0020 in XML file so we put a token in the XML
+	 * (except for HTAB, CR & LF)
+	 * The tokens we have chosen are the Unicode Characters 'SYMBOL FOR xxx' (U+2400 onwards)
+	 * Here we prepare the symbols mapping to the actual characters.
+	 */
+	private function prepareDecodeParameters() {
+		$needles = array(' ', mb_convert_encoding('&#9229;&#9226;', 'UTF-8', 'HTML-ENTITIES'));
+		$substitutes = array(' ', chr(13).chr(10));
+		for ($i = 0; $i < 32; $i++) {
+			// PHP bug doesn't allow us to use hex notation (http://bugs.php.net/48645)
+			$entity = mb_convert_encoding('&#'.(string) (9216 + $i).';', 'UTF-8', 'HTML-ENTITIES');
+			$needles[] = $entity;
+			$substitutes[] = chr($i);
+		}
+		$this->searchChars = $needles;
+		$this->replaceChars	= $substitutes;
+	}
+
+	/**
+	 * @return  boolean
+	 */
+	public function hasInternet() {
+		if ($this->hasInternet === NULL) {
+			// The @ operator is used here to avoid DNS errors when there is no connection.
+			$sock = @fsockopen("www.google.com", 80, $errno, $errstr, 1);
+			$this->hasInternet = (bool) $sock ? TRUE : FALSE;
+		}
+		return $this->hasInternet;
 	}
 }
